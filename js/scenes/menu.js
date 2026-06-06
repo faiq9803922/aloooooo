5060112e9dee6ff8dc710fdbde01846fc21fa95c
faiq9803922 <faiqahmad3218@gmail.com>
2026-06-06 13:39:18 +0500

    Update menu.js

--- js/scenes/menu.js
+++ js/scenes/menu.js
@@ -1,1 +1,20 @@
 // UZELVS Shadow Revenge
+const MenuScene = {
+
+    update(Game, ctx) {
+        if (Game.keys["Enter"]) {
+            changeScene(Level1Scene);
+        }
+    },
+
+    render(Game, ctx) {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "white";
+        ctx.font = "40px Arial";
+        ctx.textAlign = "center";
+
+        ctx.fillText("START GAME (Press ENTER)", canvas.width/2, canvas.height/2);
+    }
+};
