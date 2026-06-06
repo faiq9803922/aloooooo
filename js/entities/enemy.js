8dd7f11275664e406f0ca2fbdcf2813a5cbedb61
faiq9803922 <faiqahmad3218@gmail.com>
2026-06-06 14:02:45 +0500

    Update enemy.js

--- js/entities/enemy.js
+++ js/entities/enemy.js
@@ -1,1 +1,26 @@
 // UZELVS Shadow Revenge
+class Enemy {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.w = 40;
+        this.h = 40;
+        this.hp = 5;
+    }
+
+    update(Game) {
+        if (Game.player) {
+            if (Game.player.x < this.x) this.x -= 1;
+            if (Game.player.x > this.x) this.x += 1;
+        }
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = this.hp > 0 ? "red" : "gray";
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+
+        // health bar
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.x, this.y - 10, this.hp * 8, 5);
+    }
+}
