80155feaf6bae8532253fcf446d749507f86e3f6
faiq9803922 <faiqahmad3218@gmail.com>
2026-06-06 13:39:31 +0500

    Update player.js

--- js/entities/player.js
+++ js/entities/player.js
@@ -1,1 +1,21 @@
 // UZELVS Shadow Revenge
+class Player {
+    constructor() {
+        this.x = 100;
+        this.y = 300;
+        this.speed = 4;
+        this.size = 40;
+    }
+
+    update(Game) {
+        if (Game.keys["ArrowRight"]) this.x += this.speed;
+        if (Game.keys["ArrowLeft"]) this.x -= this.speed;
+        if (Game.keys["ArrowUp"]) this.y -= this.speed;
+        if (Game.keys["ArrowDown"]) this.y += this.speed;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+}
