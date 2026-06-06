e9f174032bf14f479d83abdc09a5dd05bb15fb27
faiq9803922 <faiqahmad3218@gmail.com>
2026-06-06 14:00:43 +0500

    Update player.js

--- js/entities/player.js
+++ js/entities/player.js
@@ -1,10 +1,11 @@
 // UZELVS Shadow Revenge
 class Player {
     constructor() {
-        this.x = 100;
+        this.x = 200;
         this.y = 300;
+        this.w = 40;
+        this.h = 40;
         this.speed = 4;
-        this.size = 40;
     }
 
     update(Game) {
@@ -15,7 +16,12 @@ class Player {
     }
 
     draw(ctx) {
-        ctx.fillStyle = "white";
-        ctx.fillRect(this.x, this.y, this.size, this.size);
+        ctx.fillStyle = "black"; // shadow character
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+
+        // glowing eyes effect
+        ctx.fillStyle = "cyan";
+        ctx.fillRect(this.x + 10, this.y + 10, 5, 5);
+        ctx.fillRect(this.x + 25, this.y + 10, 5, 5);
     }
 }
