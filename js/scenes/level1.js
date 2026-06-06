bdaabf82b95f0d309a9cff0c0aab6c9c2ef364b6
faiq9803922 <faiqahmad3218@gmail.com>
2026-06-06 13:39:51 +0500

    Update level1.js

--- js/scenes/level1.js
+++ js/scenes/level1.js
@@ -1,1 +1,17 @@
 // UZELVS Shadow Revenge
+const Level1Scene = {
+    init() {
+        Game.player = new Player();
+    },
+
+    update(Game, ctx) {
+        Game.player.update(Game);
+    },
+
+    render(Game, ctx) {
+        ctx.fillStyle = "#050505";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        Game.player.draw(ctx);
+    }
+};
