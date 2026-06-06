9da664ca81637aa7c150ec591cfd682d701691e1
faiq9803922 <faiqahmad3218@gmail.com>
2026-06-06 14:04:56 +0500

    Update player.js

--- js/entities/player.js
+++ js/entities/player.js
@@ -8,54 +8,36 @@ class Player {
 
         this.speed = 4;
 
-        // combat
         this.isAttacking = false;
         this.attackTimer = 0;
         this.attackCooldown = 0;
     }
 
     update(Game) {
-
-        // movement
         if (Game.keys["ArrowRight"]) this.x += this.speed;
         if (Game.keys["ArrowLeft"]) this.x -= this.speed;
         if (Game.keys["ArrowUp"]) this.y -= this.speed;
         if (Game.keys["ArrowDown"]) this.y += this.speed;
 
-        // punch (SPACE)
+        // punch
         if (Game.keys[" "] && this.attackCooldown === 0) {
             this.isAttacking = true;
             this.attackTimer = 10;
             this.attackCooldown = 30;
         }
 
-        // attack timing
-        if (this.attackTimer > 0) {
-            this.attackTimer--;
-        } else {
-            this.isAttacking = false;
-        }
+        if (this.attackTimer > 0) this.attackTimer--;
+        else this.isAttacking = false;
 
-        if (this.attackCooldown > 0) {
-            this.attackCooldown--;
-        }
+        if (this.attackCooldown > 0) this.attackCooldown--;
 
-        // hit enemy (simple check)
-        if (this.isAttacking && Game.enemies) {
-            Game.enemies.forEach(enemy => {
-                if (
-                    this.x < enemy.x + enemy.w &&
-                    this.x + this.w > enemy.x &&
-                    this.y < enemy.y + enemy.h &&
-                    this.y + this.h > enemy.y
-                ) {
-                    enemy.hp -= 1;
-                }
-            });
-        }
+        // keep inside screen
+        this.x = Math.max(0, Math.min(window.innerWidth - this.w, this.x));
+        this.y = Math.max(0, Math.min(window.innerHeight - this.h, this.y));
     }
 
     draw(ctx) {
+        // DARK SHADOW PLAYER (NOT WHITE)
         ctx.fillStyle = "black";
         ctx.fillRect(this.x, this.y, this.w, this.h);
 
@@ -64,9 +46,10 @@ class Player {
         ctx.fillRect(this.x + 10, this.y + 10, 5, 5);
         ctx.fillRect(this.x + 25, this.y + 10, 5, 5);
 
-        // punch effect
+        // punch glow
         if (this.isAttacking) {
             ctx.strokeStyle = "white";
+            ctx.lineWidth = 2;
             ctx.strokeRect(this.x - 10, this.y - 10, this.w + 20, this.h + 20);
         }
     }
